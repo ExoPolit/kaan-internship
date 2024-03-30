2a504572c219ab7ad923b90a26215a232410a976
Andrew-Karvelis <akarvelis.work@gmail.com>
2024-03-30 13:36:46 +1100

    (FIX) Skeleton loading state loading properly with correct dims.

--- src/components/home/NewItems.jsx
+++ src/components/home/NewItems.jsx
@@ -64,61 +64,27 @@ const NewItems = () => {
               >
                 {[...Array(4)].map((_, index) => (
                   <div key={index}>
-                    <div className="nft__item">
-                      <Skeleton
-                        className="lazy img-fluid"
-                        width="300px"
-                        height="350px"
-                      />
                       <div className="author_list_pp">
                         <Skeleton
                           width="60px"
                           height="60px"
-                          border-radius="50%"
-                          className="lazy"
                           alt=""
                         />
+                      <i className="fa fa-check"></i>
                       </div>
-                        <i className="fa fa-check"></i>
-                      <div className="de_countdown">
-                        <Skeleton width="30px" height="15px" />
-                      </div>
+                      
+                    <div className="nft__item">
+                      <Skeleton
+                        className="lazy img-fluid"
+                        width="275px"
+                        height="300px"
+                      />
 
-                      <div className="nft__item_wrap">
-                        <div className="nft__item_extra">
-                          <div className="nft__item_buttons">
-                            <button>Buy Now</button>
-                            <div className="nft__item_share">
-                              <h4>Share</h4>
-                              <a href="" target="_blank" rel="noreferrer">
-                                <i className="fa fa-facebook fa-lg"></i>
-                              </a>
-                              <a href="" target="_blank" rel="noreferrer">
-                                <i className="fa fa-twitter fa-lg"></i>
-                              </a>
-                              <a href="">
-                                <i className="fa fa-envelope fa-lg"></i>
-                              </a>
-                            </div>
-                          </div>
-                        </div>
-
-                        <div>
-                          <Skeleton
-                            width="160px"
-                            height="15px"
-                            className="lazy nft__item_preview"
-                          />
-                        </div>
-                      </div>
                       <div className="nft__item_info">
                         <Skeleton width="160px" height="15px" />
                         <h4>
                           <Skeleton width="60px" height="15px" />
                         </h4>
-                        <div className="nft__item_price">
-                          <Skeleton width="30px" height="15px" />
-                        </div>
                         <div className="nft__item_like">
                           <i className="fa fa-heart"></i>
                           <span>
@@ -161,10 +127,10 @@ const NewItems = () => {
                   <div className="nft__item">
                     <div className="author_list_pp">
                       <Link
-                        to={`/author/${item.authorId}`}
+                        to={`/author`}
                         data-bs-toggle="tooltip"
                         data-bs-placement="top"
-                        title={`Creator: ${item.authorId}`}
+                        title={`Creator:`}
                       >
                         <img className="lazy" src={item.authorImage} alt="" />
                         <i className="fa fa-check"></i>
