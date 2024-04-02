36bf09d90d1ef82d076c9009424d5f7738af0d75
Andrew-Karvelis <akarvelis.work@gmail.com>
2024-04-02 11:31:44 +1100

    (ADD) Skeleton loading state & links to profiles

--- src/components/home/TopSellers.jsx
+++ src/components/home/TopSellers.jsx
@@ -1,6 +1,7 @@
 import React, { useState, useEffect } from "react";
 import { Link } from "react-router-dom";
 import axios from "axios";
+import Skeleton from "../UI/Skeleton";
 
 const TopSellers = () => {
   const [items, setItems] = useState([]);
@@ -13,7 +14,6 @@ const TopSellers = () => {
       )
       .then(function (response) {
         setItems(response.data);
-        console.log(response.data)
         setLoading(false);
       })
       .catch(function (error) {
@@ -32,27 +32,54 @@ const TopSellers = () => {
             </div>
           </div>
           <div className="col-md-12">
-            <ol className="author_list">
+            {loading ? (
+              <>
+                <ol className="author_list">
+                  {items.map((item, index) => (
+                    <li key={index}>
+                      <div className="author_list_pp">
+                        <Skeleton
+                          width="60px"
+                          height="60px"
+                          borderRadius="50px"
+                        />
+                          <i className="fa fa-check"></i>
+                      </div>
+                      <div className="author_list_info">
+                        <Skeleton 
+                        width="100px"
+                        height="15px"
+                        />
+                        <span><Skeleton width="50px" height="15px"/></span>
+                      </div>
+                    </li>
+                  ))}
+                </ol>
+              </>
+            ) : (
+              <ol className="author_list">
                 {items.map((item, index) => (
-
-                <li key={index}>
-                  <div className="author_list_pp">
-                    <Link to={`/author/${item.authorId}`}>
-                      <img
-                        className="lazy pp-author"
-                        src={item.authorImage}
-                        alt=""
-                      />
-                      <i className="fa fa-check"></i>
-                    </Link>
-                  </div>
-                  <div className="author_list_info">
-                    <Link to={`/author/${item.authorId}`}>{item.authorName}</Link>
-                    <span>{item.price} ETH</span>
-                  </div>
-                </li>
-              ))}
-            </ol>
+                  <li key={index}>
+                    <div className="author_list_pp">
+                      <Link to={`/author/${item.authorId}`}>
+                        <img
+                          className="lazy pp-author"
+                          src={item.authorImage}
+                          alt=""
+                        />
+                        <i className="fa fa-check"></i>
+                      </Link>
+                    </div>
+                    <div className="author_list_info">
+                      <Link to={`/author/${item.authorId}`}>
+                        {item.authorName}
+                      </Link>
+                      <span>{item.price} ETH</span>
+                    </div>
+                  </li>
+                ))}
+              </ol>
+            )}
           </div>
         </div>
       </div>
